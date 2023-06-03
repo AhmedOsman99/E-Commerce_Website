85fc299eb762e466d53422398d263b44a6cbd36b
[abdelrahman sabry] <[abdelrhmansabry.219@gmail.com]>
2023-06-03 16:42:52 +0300

    edite default

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,19 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Products from './components/Products';
-import ProductDetiles from './components/ProductDetiles';
-import { Navbar } from 'react-bootstrap';
-import ProductForm from './components/ProductForm';
+import { Products } from './components/Products';
+import { ProductDetails } from './components/ProductDetiles';
+import { ProductForm } from './components/ProductForm';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Routes>
         <Route path='products' element={<Products />} />
-        <Route path='products/:id' element={<ProductDetiles />} />
+        <Route path='products/:id' element={<ProductDetails />} />
         <Route path='products/:id/edit' element={<ProductForm />} />
-
       </Routes>    </div>
   );
 }
