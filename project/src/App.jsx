34ca21f074c89e77e0200947da23a92a0c00034e
eslam <eslamhassannew@gmail.com>
2023-06-03 15:53:32 +0300

    home

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,8 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+
 import { Home } from './components/Home';
 import { Login } from './components/Login';
 import { Mynav } from './components/Mynav';
+import Products from './components/Products';
+import ProductDetiles from './components/ProductDetiles';
+
+import ProductForm from './components/ProductForm';
 
 function App() {
   return (
@@ -11,12 +16,14 @@ function App() {
      
       
      <Routes>
+     <Route path='products' element={<Products />} />
+        <Route path='products/:id' element={<ProductDetiles />} />
+        <Route path='products/:id/edit' element={<ProductForm />} />
       <Route path='' element={<Home/>}/>
       <Route path='Home' element={<Home/>}/>
       
       </Routes> 
     </div>
-  );
-}
 
-export default App;
+  )}
+  export default App;
